7b49c4df677c6f21c5dc94cfd4fa2cc4e15c7be3
redebron20 <redebron20@gmail.com>
2020-10-21 23:06:08 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,15 +14,15 @@
 
 def speak_to_grandma(phrase)
     if phrase == "Hi Nana, how are you?"
-        return "HUH?! SPEAK UP, SONNY!"
+        return "Huh?! Speak up, Sonny!".upcase
     elsif phrase == "Hi!"
-        return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == "WHAT DID YOU EAT TODAY?"
-        return "NO, NOT SINCE 1938!"
-    elsif phrase == "WHAT?"
-        return "NO, NOT SINCE 1938!"
-    else phrase == "I LOVE YOU GRANDMA!"
-        return "I LOVE YOU TOO PUMPKIN!"
+        return "Huh?! Speak up, Sonny!".upcase
+    elsif phrase == "What did you eat today?".upcase
+        return "No, not since 1938!".upcase
+    elsif phrase == "What?".upcase
+        return "No, not since 1938!".upcase
+    else phrase == "I love you grandma!".upcase
+        return "I love you too pumpkin!".upcase
     end
 
 end
